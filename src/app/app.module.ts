11c3113b96837f83cc9241b14d6ed88409615ae9
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-03-22 07:33:08 +0530

    Added router to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
@@ -19,7 +20,8 @@ import { ContentComponent } from './content/content.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    CustomMaterialModule
+    CustomMaterialModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
